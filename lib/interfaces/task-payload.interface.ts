477c637afdf665357b02c570470337a694ccfe85
José Cleiton <jcleitonbc@gmail.com>
2020-06-12 15:44:36 -0300

    fix: custom fields interface have optional fields

--- lib/interfaces/task-payload.interface.ts
+++ lib/interfaces/task-payload.interface.ts
@@ -1,15 +1,15 @@
 import { TaskPriority } from '../enums/task-priority.enum';
 
 interface CustomField {
-  status: string;
-  status_type: string;
-  id: string;
+  status?: string;
+  status_type?: string;
+  id?: string;
   name: string;
-  type: string;
-  type_config: Record<string, unknown>;
-  hide_from_guests: boolean;
-  v1_field: boolean;
-  value_deleted: string;
+  type?: string;
+  type_config?: Record<string, unknown>;
+  hide_from_guests?: boolean;
+  v1_field?: boolean;
+  value_deleted?: string;
 }
 
 export interface TaskPayload {
