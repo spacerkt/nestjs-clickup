6509887a1809f6100da9a93bb4f8d5b5a41026a1
José Cleiton <jcleitonbc@gmail.com>
2020-10-23 15:46:53 -0300

    fix: provide oauth at clickup service

--- lib/clickup.service.ts
+++ lib/clickup.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { OAuthProvider } from './providers/oauth.provider';
 import { TaskProvider } from './providers/task.provider';
 
 @Injectable()
 export class ClickUpService {
-  constructor(readonly task: TaskProvider) {}
+  constructor(readonly task: TaskProvider, readonly oauth: OAuthProvider) {}
 }
